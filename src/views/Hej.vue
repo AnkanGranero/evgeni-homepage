003a421e9cbe5813e0f8a407fa17d04f4086d02c
Andreas Graner <grannemannen@gmail.com>
2021-02-28 21:01:00 +0100

    age calc

--- src/views/Hej.vue
+++ src/views/Hej.vue
@@ -5,15 +5,36 @@
       <div class="body-content">
         <img src="images/Hej.jpg" id="hej" />
 
-        <p>Jag heter Evgeni Leonov och är 32 år gammal. Jag utbildade mig till skådespelare på Teaterhögskolan i Stockholm 2007-2010. Sedan jag blev färdig med utbildningen har jag arbetat som frilansande skådespelare i olika delar av Sverige. B.la. på Smålands musik och teater och Teateri i Jönköping och på 4:e teatern i Västerås. Jag har medverkat i många föreställningar som varit riktade till ungdomar och turnerat runt om i Sverige i flera år. En av dessa föreställningar har varit en egenskriven monologföreställning som jag har spelat i både Sverige och Ryssland på svenska, ryska och spanska. Jag har även medverkat i film och reklam, b.la. Johan Falk och Ikea.</p>
         <p>
-          På Stockholms dramatiska högskola har jag gått kurserna Filmskådespeleri 1 och Filmskådespeleri 2 som leddes av Matthew Allen. Där fick jag lära mig mer om filmmanusarbete, regi, klippning och filmande. Inspirerad av kurserna började jag arbeta bakom kameran. Jag har gjort två kampanjfilmer för Kompis Sverige, flera musikvideos till mitt band och håller nu på
-          att färdigställa en dokumentärfilm om min familjs flykt från Pinochetregimen i Chile.
+          Jag heter Evgeni Leonov och är {{ age }} år gammal. Jag utbildade mig
+          till skådespelare på Teaterhögskolan i Stockholm 2007-2010. Sedan jag
+          blev färdig med utbildningen har jag arbetat som frilansande
+          skådespelare i olika delar av Sverige. B.la. på Smålands musik och
+          teater och Teateri i Jönköping och på 4:e teatern i Västerås. Jag har
+          medverkat i många föreställningar som varit riktade till ungdomar och
+          turnerat runt om i Sverige i flera år. En av dessa föreställningar har
+          varit en egenskriven monologföreställning som jag har spelat i både
+          Sverige och Ryssland på svenska, ryska och spanska. Jag har även
+          medverkat i film och reklam, b.la. Johan Falk och Ikea.
+        </p>
+        <p>
+          På Stockholms dramatiska högskola har jag gått kurserna
+          Filmskådespeleri 1 och Filmskådespeleri 2 som leddes av Matthew Allen.
+          Där fick jag lära mig mer om filmmanusarbete, regi, klippning och
+          filmande. Inspirerad av kurserna började jag arbeta bakom kameran. Jag
+          har gjort två kampanjfilmer för Kompis Sverige, flera musikvideos till
+          mitt band och håller nu på att färdigställa en dokumentärfilm om min
+          familjs flykt från Pinochetregimen i Chile.
         </p>
 
         <p>
-          Jag sjunger, spelar och komponerar låtar i bandet Dreaming of Space och arbetar som musikledare på daglig verksamhet. Parallellt med mitt konstnärskap har jag utbildat mig till rysk – svensk kontakttolk och arbetar i offentlig sektor. Jag har även ett starkt samhällsengagemang och arbetar som volontär för Kompis Sverige och är även politiskt aktiv. Jag älskar att träffa nya människor, utvecklas och lära mig nya saker.
-          Hör gärna av dig om du vill samarbeta.
+          Jag sjunger, spelar och komponerar låtar i bandet Dreaming of Space
+          och arbetar som musikledare på daglig verksamhet. Parallellt med mitt
+          konstnärskap har jag utbildat mig till rysk – svensk kontakttolk och
+          arbetar i offentlig sektor. Jag har även ett starkt samhällsengagemang
+          och arbetar som volontär för Kompis Sverige och är även politiskt
+          aktiv. Jag älskar att träffa nya människor, utvecklas och lära mig nya
+          saker. Hör gärna av dig om du vill samarbeta.
         </p>
       </div>
     </div>
@@ -22,7 +43,14 @@
 
 <script>
 export default {
-  name: "Hej"
+  name: "Hej",
+  computed: {
+    age() {
+      const diff_ms = Date.now() - new Date(1987, 2, 6).getTime();
+      const age_dt = new Date(diff_ms);
+      return Math.abs(age_dt.getUTCFullYear() - 1970);
+    },
+  },
 };
 </script>
 
